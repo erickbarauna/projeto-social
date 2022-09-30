a974f6663956b3c158a9d6d587a3684a48873863
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-09-30 19:47:12 -0300

    .

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,6 @@ const menuToggle = menuSection.querySelector(".menu-toggle");
 
 
 function Clique() {
-    document.body.style.overflow = show ? "initial" : "hidden"
-
     document.querySelector(".menu-section").classList.toggle("on", show) // Essa função toggle põe ou tira uma class da tag, no caso (".menu-section")
     show = !show;
 }
