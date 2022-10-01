ab51867855cfd64c1c5887bc64b14e15834534ac
Joyce Pereira Oliveira <98670029+erickbarauna@users.noreply.github.com>
2022-10-01 12:27:04 -0300

    Projeto Finalizado

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,6 @@ let show = true
 
 const menuToggle = menuSection.querySelector(".menu-toggle");
 
-
 function Clique() {
     document.querySelector(".menu-section").classList.toggle("on", show) // Essa função toggle põe ou tira uma class da tag, no caso (".menu-section")
     show = !show;
